2e636e6d18e3f256e7bdfd3cee90de39340a825a
Kavinkumar N <168163280+Kevinxkavin@users.noreply.github.com>
2026-02-25 12:23:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from routes import main
 
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:KR76181%40zz@localhost/nvd_db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:password@localhost/nvd_db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db.init_app(app)
